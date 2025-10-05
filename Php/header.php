c99498f26923f863468517d6f2e299da49b0a38e
jesus-justin <mjesusjustin@gmail.com>
2025-10-05 11:22:28 +0800

    added header

---
+++ Php/header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Buyanihan - E-Commerce Website</title>
+  <link rel="stylesheet" href="../css/<?php echo basename($_SERVER['PHP_SELF'], '.php'); ?>.css">
+</head>
+<body>
+  <header>
+    <div class="navbar">
+      <h1><a href="index.php">Buyanihan</a></h1>
+      <nav>
+        <a href="products.php">Products</a>
+        <a href="cart.php">Cart</a>
+        <a href="order_history.php">Orders</a>
+      </nav>
+    </div>
+  </header>
+  <main>
