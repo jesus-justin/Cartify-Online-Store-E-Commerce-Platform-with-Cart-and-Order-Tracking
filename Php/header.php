65c138c69dbe4eb2f583ae793b3c5a54764dbd0b
jesus-justin <mjesusjustin@gmail.com>
2025-10-11 20:38:38 +0800

    css part 2

--- Php/header.php
+++ Php/header.php
@@ -4,16 +4,19 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PinoyTech Finds</title>
-    <link rel="stylesheet" href="../css/index.css">
+    <link rel="stylesheet" href="../css/header.css">
 </head>
 <body>
     <header>
         <h1>🛍️ PinoyTech Finds</h1>
-        <nav>
-            <a href="index.php">Home</a>
-            <a href="products.php">Products</a>
-            <a href="cart.php">Cart</a>
-            <a href="checkout.php">Checkout</a>
-            <a href="order_history.php">Orders</a>
+        <nav class="nav-buttons">
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="products.php">Products</a></li>
+                <li><a href="cart.php">Cart</a></li>
+                <li><a href="checkout.php">Checkout</a></li>
+                <li><a href="order_history.php">Orders</a></li>
+            </ul>
         </nav>
     </header>
+    <script src="../js/effects.js"></script>
